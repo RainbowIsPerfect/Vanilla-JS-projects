341889304baa6fc6bfb4b126fa6508c1acd36372
RainbowIsPerfect <andrey-smollin@rambler.ru>
2023-02-09 19:55:08 +0300

    few changes

--- tabs/main.js
+++ tabs/main.js
@@ -6,17 +6,9 @@ tabsList.forEach(tab => tab.addEventListener('click', changeTab));
 function changeTab() {
     const currentTabDataSet = this.dataset.tab;
     tabsList.forEach(tab => {
-        if (tab !== this) {
-            tab.classList.remove('tabs__button--active');
-        } else {
-            this.classList.add('tabs__button--active');
-        }
+        tab !== this ? tab.classList.remove('tabs__button--active') : this.classList.add('tabs__button--active');
     });
     contentList.forEach(content => {
-        if (content.dataset.content === currentTabDataSet) {
-            content.classList.remove('tabs__content--hidden');
-        } else {
-            content.classList.add('tabs__content--hidden');
-        }
+        content.dataset.content === currentTabDataSet ? content.classList.add('tabs__content--active') : content.classList.remove('tabs__content--active');
     });
 }
